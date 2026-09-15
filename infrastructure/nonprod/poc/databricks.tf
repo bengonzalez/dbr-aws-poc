06b1781fe33665d04d3504835118c392002ddd23
Benjamin <bengonzalezre@gmail.com>
2026-09-15 18:32:59 -0500

    Create UC external location

--- infrastructure/nonprod/poc/databricks.tf
+++ infrastructure/nonprod/poc/databricks.tf
@@ -15,3 +15,18 @@ resource "databricks_storage_credential" "data_access" {
 
   comment = "Unity Catalog storage credential for ${var.project_name} S3 data"
 }
+
+########################################################
+#
+# Unity Catalog External Location
+#
+########################################################
+
+resource "databricks_external_location" "data" {
+  provider        = databricks.workspace
+  name            = "${var.project_name}-data"
+  url             = "s3://${module.storage.data_bucket_id}/"
+  credential_name = databricks_storage_credential.data_access.name
+  comment         = "Unity Catalog external location for ${var.project_name} S3 data"
+  skip_validation = false
+}
