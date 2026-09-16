7873af398253c8c70592b523e29c6f1955796b93
Benjamin <bengonzalezre@gmail.com>
2026-09-16 17:18:54 -0500

    Add validation components, catalog, schema, and table

--- infrastructure/nonprod/poc/databricks.tf
+++ infrastructure/nonprod/poc/databricks.tf
@@ -30,3 +30,58 @@ resource "databricks_external_location" "data" {
   comment         = "Unity Catalog external location for ${var.project_name} S3 data"
   skip_validation = false
 }
+
+########################################################
+#
+# Unity Catalog POC Catalog
+#
+########################################################
+
+resource "databricks_catalog" "poc" {
+  provider = databricks.workspace
+
+  name    = "${var.project_name}_catalog"
+  comment = "Unity Catalog catalog for the ${var.project_name} POC"
+
+  storage_root = "s3://${module.storage.data_bucket_id}/managed/"
+}
+
+########################################################
+#
+# Unity Catalog POC Schema
+#
+########################################################
+
+resource "databricks_schema" "poc" {
+  provider = databricks.workspace
+
+  catalog_name = databricks_catalog.poc.name
+  name         = "poc_schema"
+  comment      = "Schema for Databricks POC validation"
+}
+
+########################################################
+#
+# Unity Catalog Managed Validation Table
+#
+########################################################
+
+resource "databricks_sql_table" "validation" {
+  provider = databricks.workspace
+
+  name               = "validation_table"
+  catalog_name       = databricks_catalog.poc.name
+  schema_name        = databricks_schema.poc.name
+  table_type         = "MANAGED"
+  data_source_format = "DELTA"
+
+  column {
+    name = "id"
+    type = "INT"
+  }
+
+  column {
+    name = "message"
+    type = "STRING"
+  }
+}
